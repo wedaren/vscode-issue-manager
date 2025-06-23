f4922e57a7aab0cad66c608c3261259d34161e48
gao.weicheng <gao.weicheng@yottabyte.cn>
2025-06-23 17:05:28 +0800

    i18n: Translate test file to Chinese

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,14 +1,14 @@
 import * as assert from 'assert';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
+// 您可以从“vscode”模块导入和使用所有 API
+// 以及导入您的扩展以对其进行测试
 import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+suite('扩展测试套件', () => {
+	vscode.window.showInformationMessage('开始所有测试。');
 
-	test('Sample test', () => {
+	test('示例测试', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
 	});
