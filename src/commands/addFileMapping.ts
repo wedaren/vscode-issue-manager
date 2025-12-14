3d2dd8f9191bdead2c903d0b5933571dc234a9f0
wedaren 14 <wedaren@gmail.com>
2025-12-14 14:42:05 +0800

    移除未使用的路径模块导入

--- src/commands/addFileMapping.ts
+++ src/commands/addFileMapping.ts
@@ -1,5 +1,4 @@
 import * as vscode from 'vscode';
-import * as path from 'path';
 import { NoteMappingService } from '../services/noteMapping/NoteMappingService';
 import { QuickPickNoteSelector } from '../ui/QuickPickNoteSelector';
 import { getIssueDir } from '../config';
